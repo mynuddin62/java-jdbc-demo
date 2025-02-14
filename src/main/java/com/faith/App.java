12ffe642072c3476597510a1cb4090fb33236b54
mynuddin62 <ratulmynuddin@yahoo.com>
2025-02-14 10:53:44 +0600

    initial commit

---
+++ src/main/java/com/faith/App.java
@@ -0,0 +1,13 @@
+package com.faith;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
